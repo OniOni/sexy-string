36f586d3bd87bc1ae696f735aa3b7d77cb918715
OniOni <mathieu.c.sabourin@gmail.com>
2012-06-25 11:49:42 -0700

    Added some tests.

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 var should = require('should'),
 color = require('./lib/main');
 
-"hey".times(3).should.equal('heyheyhey');
+"hey".should.be.a('string');
+"hey".times(3).should.be.a('string');
 
-console.log("hey".times(3).grey());
-console.log("this is red".red());
-console.log("this is green".green());
-console.log("this is red".color('red'));
+"hey".times(3).should.equal('heyheyhey');
+"hey".times(3).grey().should.equal('\033[37mheyheyhey\033[0m');
 
 "this is it".contains('is').should.equal(true);
+"this is it".contains('not').should.equal(false);
 
-"".contains('is').should.equal(false);
+"This should contain the string we are looking for".contains('the string we are looking for').should.be.ok;
+"And this one should not contain the string we are looking for".contains('the string we are not looking for').should.not.be.ok;
+
+"".contains('is').should.not.be.ok;
 
